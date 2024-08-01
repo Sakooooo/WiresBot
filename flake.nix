ab8c3e86312230c049ee611480ac5240df090c85
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-08-01 22:11:30 +0400

    hmmm

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
     devShells = eachSystem (system: {
       default = pkgsFor.${system}.mkShell {
         name = "wiresbot-shell";
-        packages = with pkgsFor.${system}; [go gopls gotools go-tools imagemagickBig];
+        packages = with pkgsFor.${system}; [go gopls gotools go-tools imagemagickBig pkgconfig];
       };
     });
 
