7c4b39cf4f3533686df04fbeb7ccb07f0e4775e3
NotAShelf <raf@notashelf.dev>
2024-08-01 17:21:55 +0300

    nix: build via buildGoModule

---
+++ nix/package.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  buildGoModule,
+  date,
+  ...
+}:
+buildGoModule {
+  pname = "wiresbot-go";
+  version = "0-unstable-${date}";
+
+  # Reproducible source path
+  # pass a filter to get rid of irrelevant files
+  # in the source path
+  src = builtins.path {
+    name = "wiresbot-go";
+    path = ../.;
+  };
+
+  vendorHash = "sha256-nyB3juOxyrp+CuEpwKoBr23Py9S67FhUYg8sjLTBDPI=";
+  ldflags = ["-s" "-w"];
+
+  meta = {
+    description = "Wired";
+    homepage = "https://github.com/Sakooooo/WiresBot";
+    license = lib.licenses.gpl3Only;
+  };
+}
